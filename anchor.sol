f10c9366efa01f2eaa79fbd580c680fbc6a42ea2
horvendile <horvendile@montfalcon.co.uk>
2017-12-19 15:14:05 +0000

    Update for maxWager

--- anchor.sol
+++ anchor.sol
@@ -1,99 +1,87 @@
 pragma solidity ^0.4.11;
 
-contract Anchor {
+contract HEX {
 
-    address public owner;
+  address public owner;
 
-    uint counter;
-    uint dieRoll;
-    uint pot;
-    bytes stakes;
-    uint[] hit;
-    uint i;
-//--------------------------------------------------------------------------
-    event Deposit(address _sender, uint _value, uint _stake, bytes _message);
-    event Sent(address indexed _receiver, uint _value);
-    event Stuff(uint _blockNumber, uint _timestamp, uint _digits, address _sender, uint _stake, bytes _message );
-    event Score(uint _number);
+  uint counter;
+  uint dieRoll;
+  uint pot;
+  uint[] hit;
+  uint i;
 
-    // Constrctor function
-    function Anchor() payable public {
-        owner = msg.sender;
-        pot = msg.value;
-        Deposit(owner, pot, 0, msg.data);
+  //    event Deposit(address _sender, uint _value, uint _stake, bytes _message);
+  event Sent(address indexed _receiver, uint _value);
+  event HashSource(uint _hs);
+
+  // Constrctor function
+  function HEX() payable public {
+    owner = msg.sender;
+    pot = msg.value;
+  }
+
+
+  function transferPayment(uint payment, address recipient) internal {
+    recipient.transfer(payment);
+    Sent(recipient, payment);
+  }
+
+  function withdraw(uint _amount) public {
+    if (msg.sender==owner)
+    transferPayment(_amount, owner);
+  }
+
+  function kill() public {
+    if (msg.sender==owner)
+    selfdestruct(owner);
+  }
+
+
+  function() public payable {
+
+    // Calculate and test stakes
+    {
+
+      if (msg.value == 0) revert();
+
+      uint stakeCount = 0;
+      for ( i = 0 ; i < 16 ; i++ ) {
+        stakeCount += uint(msg.data[i]);
+      }
+
+      uint singleStake = msg.value / stakeCount;
+
+      if (singleStake > pot/10) revert();
+      if (singleStake > 10**18) revert();
+
+      //            Deposit(msg.sender, msg.value, singleStake, msg.data);
     }
 
+    hit = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
 
-    function transferPayment(uint payment, address recipient) internal {
-        recipient.transfer(payment);
-        Sent(recipient, payment);
-    }
+    counter = counter + 1;
 
-    function withdraw(uint _amount) public {
-        if (msg.sender==owner)
-        transferPayment(_amount, owner);
-    }
+    uint sh = uint(keccak256(block.number,block.blockhash(block.number-1), block.timestamp,counter,msg.sender,msg.value,msg.data));
+    HashSource( sh );
 
-    function kill() public {
-         if (msg.sender==owner)
-         selfdestruct(owner);
-    }
+    uint total = 0;
+    uint hitTotal = 0;
+    for ( i = 0; i < 8; i++) {
+      dieRoll = sh % 16;
+      sh = sh / 16;
 
-
-    function() public payable {
-
-        // Calculate and test stakes
-        {
-
-            if (msg.value == 0) revert();
-
-            uint totalStake = msg.value;
-            stakes = msg.data;
-
-            uint stakeCount = 0;
-           for ( i = 0 ; i < 16 ; i++ ) {
-               stakeCount += uint(stakes[i]);
-           }
-
-           uint singleStake = totalStake / stakeCount;
-
-            if (singleStake > pot/10) revert();
-
-            Deposit(msg.sender, totalStake, singleStake, msg.data);
+      if (uint(msg.data[dieRoll]) == 1) {
+        if (hit[dieRoll] == 0 ) {
+          hit[dieRoll] = 1;
+          hitTotal++;
         }
+        total++;
+      }
 
-        address ms = msg.sender;
-
-
-        hit = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
-
-        counter = counter + 1;
-
-        uint bn = block.number;
-        uint bts = block.timestamp;
-        uint sh = uint(keccak256(bn,block.blockhash(bn-1), bts,counter,ms,totalStake,msg.data));
-
-        Stuff( bn, bts, sh, ms, totalStake, msg.data );
-
-        uint total = 0;
-        uint hitTotal = 0;
-        for ( i = 0; i < 8; i++) {
-            dieRoll = sh % 16;
-            sh = sh / 16;
-
-            if (uint(stakes[dieRoll]) == 1) {
-                if (hit[dieRoll] == 0 ) {
-                    hit[dieRoll] = 1;
-                    hitTotal++;
-                }
-            total++;
-            }
-
-            Score(dieRoll);
-        }
-        total += hitTotal;
-        uint payout = total * singleStake;
-        transferPayment(payout, ms);
-        pot = address(this).balance;
     }
+    total += hitTotal;
+    uint payout = total * singleStake;
+    transferPayment(payout, msg.sender);
+    pot = address(this).balance;
+  }
 }
